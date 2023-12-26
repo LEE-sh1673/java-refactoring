b57bdf4b21ae081383fc2ab7ec8a19204da36fdd
LEE-sh1673 <lsh901673@gmail.com>
2023-12-26 18:54:20 +0900

    test: add unit test of JsonReader

---
+++ src/test/java/book/JsonReaderTest.java
@@ -0,0 +1,31 @@
+package book;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import book.utils.JsonReader;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+class JsonReaderTest {
+
+    @Test
+    void test_read() {
+        assertThat(JsonReader.read("money.json")).contains("amount");
+        assertThat(JsonReader.read("user.json")).contains("name", "age");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void test_read_when_file_null_or_empty(final String fileName) {
+        assertThatThrownBy(() -> JsonReader.read(fileName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void test_read_when_file_non_exists() {
+        assertThatThrownBy(() -> JsonReader.read("non-exists.json"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
