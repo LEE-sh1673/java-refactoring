fc8eeea7b992c4fd993ced002f68c47b52976e8a
LEE-sh1673 <lsh901673@gmail.com>
2023-12-26 23:51:51 +0900

    refactor: PlaysDeserializer에 대한 리팩토링 진행
    변경 내용:
    - JsonParser로부터 필드 정보를 얻어오는 대신 TypeReference를 이용해 역직렬화를 수행하도록 구조를 변경
    - 클래스 및 생성자의 접근 제어자를 package-private으로 변경

--- src/test/java/book/chapter01/PlaysDeserializer.java
+++ src/test/java/book/chapter01/PlaysDeserializer.java
@@ -1,29 +1,24 @@
 package book.chapter01;
 
-import book.chapter01.Play;
-import book.chapter01.Plays;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 
-public class PlaysDeserializer extends StdDeserializer<Plays> {
+class PlaysDeserializer extends StdDeserializer<Plays> {
 
     private final ObjectMapper mapper;
 
-    public PlaysDeserializer() {
+    PlaysDeserializer() {
         this(null);
     }
 
-    protected PlaysDeserializer(final Class<?> cls) {
+    PlaysDeserializer(final Class<?> cls) {
         super(cls);
         this.mapper = new ObjectMapper()
                 .setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
@@ -34,19 +29,11 @@ public class PlaysDeserializer extends StdDeserializer<Plays> {
             final JsonParser jsonParser,
             final DeserializationContext context
     ) throws IOException {
-        final Iterator<Entry<String, JsonNode>> fields = fields(jsonParser);
-        final Map<String, Play> plays = new HashMap<>();
-
-        while (fields.hasNext()) {
-            final Map.Entry<String, JsonNode> entry = fields.next();
-            final Play play = mapper.convertValue(entry.getValue(), Play.class);
-            plays.put(entry.getKey(), play);
-        }
-        return new Plays(plays);
+        return new Plays(readValue(jsonParser));
     }
 
-    Iterator<Entry<String, JsonNode>> fields(final JsonParser parser) throws IOException {
-        final JsonNode node = parser.getCodec().readTree(parser);
-        return node.fields();
+    private Map<String, Play> readValue(final JsonParser jsonParser) throws IOException {
+        return mapper.readValue(jsonParser, new TypeReference<>() {
+        });
     }
 }
